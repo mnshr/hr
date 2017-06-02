de2dd5f36bc9d7334b0ee8e0a58509b8d02c0b58
mnshr <mhira@alumni.nd.edu>
2017-06-01 23:54:23 -0400

    added

---
+++ two_stack_Q.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 01 23:53:24 2017
+
+@author: mnshr
+"""
+
+class MyQueue(object):
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+    
+    def peek(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+        
+    def pop(self):
+        if not self.stack2: 
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+        
+    def put(self, value):
+        self.stack1.append(value)
+        
+
+queue = MyQueue()
+t = int(raw_input())
+for line in xrange(t):
+    values = map(int, raw_input().split())
+    
+    if values[0] == 1:
+        queue.put(values[1])        
+    elif values[0] == 2:
+        queue.pop()
+    else:
+        print queue.peek()
+        
