4cca223ef46425344e6fb4f18a4864e850026dfa
mnshr <mhira@alumni.nd.edu>
2017-06-03 10:48:52 -0400

    updated till session 9

--- sentdex/Intermediate.py
+++ sentdex/Intermediate.py
@@ -77,13 +77,15 @@ print(xyz[:5])
          return False
  
  xyz = (i for i in input_list if div_by_five(i))
-# print(list(xyz))
+ print(list(xyz))
  
-# xyz = [i for i in input_list if div_by_five(i)]
-# print(xyz)
-# [print (i) for i in range(5)]
-
-[[print(i,ii) for ii in range(3)] for i in range(5)]
+ xyz = [i for i in input_list if div_by_five(i)]
+ print(xyz)
+ [i for i in range(5)]
+ 
+#Range of 3 elements, 5 times in the outer loop
+#simpler form of nested loop
+[[ii for ii in range(3)] for i in range(5)]
 
 #%%
 #generator
@@ -147,3 +149,98 @@ print y
 z=['abc', '123', 1, 5] #We can have different data type elements in a list
 for i in range(len(z)):
     print z[i]
+    
+#%%
+#Using Enumerate
+example = ['left','right','up','down']
+# for i in range(len(example)):       #Enumerate is a better alternative
+#     print(i, example[i])
+
+for i,j in enumerate(example):
+    print(i,j)
+
+#Enumerate on Dict
+example_dict = {'left':'<','right':'>','up':'^','down':'v',}
+#[print(i,j) for i,j in enumerate(example_dict)]
+
+new_dict = dict(enumerate(example_dict))
+print (example_dict)
+print (new_dict) #Prints the indexes
+
+#%%
+
+#Using zip to combine elements in 2 lists with common indexes
+x = [1,2,3,4]
+y = [7,8,3,2]
+z = ['a','b','c','d']
+
+for a,b in zip(x,y):
+    print(a,b)
+    
+for a,b,c in zip(x,y,z):
+     print(a,b,c)
+
+#Creates a list of tuples     
+print(zip(x,y,z))
+#Same as above
+print(list(zip(x,y,z)))
+
+#With 2 values we can convert it to a Dict
+print(dict(zip(x,y)))
+
+#Using list comprehension
+[(a,b,c) for (a,b,c) in zip(x,y,z)]
+
+#%%
+import timeit
+#Generators yield instead of return
+def simple_gen():
+    yield 'Oh'
+    yield 'hello'
+    yield 'there'
+     
+for i in simple_gen():
+    print(i)
+
+#Generator Expr using Generator function
+print (i for i in simple_gen())
+#List comprehension using Generator function
+print [i for i in simple_gen()]
+
+CORRECT_COMBO = (3, 6, 1)
+found_c=False
+def lets_find_combo():
+    for c1 in range(10):
+    #    if found_c:
+    #        break
+        for c2 in range(10):
+    #        if found_c:
+    #            break
+            for c3 in range(10):
+                if (c1, c2, c3) == CORRECT_COMBO:
+                   print('Found the combo:{}'.format((c1, c2, c3)))
+                   found_c = True
+                   break
+    #            print (c1, c2, c3) #Goes through all the combos of c1, c2 ,c3
+                                  #if we didn't use break in all the loops
+                              
+def combo_gen():
+    for c1 in range(10):
+        for c2 in range(10):
+            for c3 in range(10):
+                yield (c1, c2, c3)
+
+def lets_use_gen():
+    for (c1, c2, c3) in combo_gen(): #generators yields tuples in a stream
+        #print(c1, c2, c3)
+        if (c1, c2, c3) == CORRECT_COMBO:
+            print('Found the combo:{}'.format((c1, c2, c3)))
+            break
+
+print timeit.timeit()
+lets_use_gen()
+print timeit.timeit()
+lets_find_combo()
+print timeit.timeit()
+
+#%%
