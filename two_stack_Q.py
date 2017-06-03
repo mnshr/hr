f636e64d95eb59bc9252dfa93050d7c004b0e244
mnshr <mhira@alumni.nd.edu>
2017-06-03 09:39:44 -0400

    updated comments

--- two_stack_Q.py
+++ two_stack_Q.py
@@ -14,16 +14,16 @@ class MyQueue(object):
         if not self.stack2:
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
-        return self.stack2[-1]
+        return self.stack2[-1] #returns the last element value
         
     def pop(self):
         if not self.stack2: 
             while self.stack1:
                 self.stack2.append(self.stack1.pop())
-        return self.stack2.pop()
+        return self.stack2.pop() #returns the last element and deletes it
         
     def put(self, value):
-        self.stack1.append(value)
+        self.stack1.append(value) #adds the element to the stack list
         
 
 queue = MyQueue()
