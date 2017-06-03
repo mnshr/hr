aad894a3e11f2771bb97aecdc09b52562d4dace1
mnshr <mhira@alumni.nd.edu>
2017-06-03 14:32:58 -0400

    updated

--- sentdex/multiproc.py
+++ sentdex/multiproc.py
@@ -9,12 +9,24 @@ import multiprocessing
 
 def spawn(num, num2):
     print('Spawned! {} -- {}'.format(num, num2)) #Need {} for format
-    
+
+def job(num):
+    return num * 2
+
 #__name__ Necessary in multiprocessing, will run only if the 
 #script is being run, if the script is called it won't run
-if __name__=='__main__': 
+if __name__=='__main1__': 
     for i in range (55):                        
         p=multiprocessing.Process(target=spawn, args=(i,i+1)) #, need though 1 arg
         p.start()
         p.join() #waits for the process to be complete, helps thread coordination
 
+#getting information back from a process
+from multiprocessing import Pool
+if __name__=='__main__':
+    p=Pool(processes=20) #number of max processes
+    data=p.map(job, range(200)) #map job to an iterable e.g. a generator here/list
+    data2=p.map(job, [1, 3, 5])
+    p.close()
+    print(data) #a list of values from job process
+    print (data2)
